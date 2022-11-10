91112088bd217efbc1fd5a2cd6bb819b1431ed13
Leo Volwahsen <volwahsenl@gmail.com>
2022-11-10 13:36:39 +0100

    test

--- car-app/src/App.js
+++ car-app/src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import Car from "./components/Car";
 
 function App() {
-  return (
-    <div className="App">
-      <Car />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Car />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Car.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Car() {
-  return <div className="car">Make your instrument cluster here</div>;
-}
