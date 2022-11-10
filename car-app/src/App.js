d4a00f850642c1fbfc13e64f6e90ec2ec9f7cbc1
Leo Volwahsen <volwahsenl@gmail.com>
2022-11-10 20:19:40 +0100

    t

--- car-app/src/App.js
+++ car-app/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Car from "./components/Car";
 
+
 function App() {
     return (
         <div className="App">
+            <h1>hello</h1>
             <Car />
+            <Speedometer id="dial5" value="-110" title="Acceleration X" />
         </div>
     );
 }
