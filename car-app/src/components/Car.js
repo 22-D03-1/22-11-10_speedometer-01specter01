bf4e6f53a28d31a16c8758dcf0e0030d1d233b05
Leo Volwahsen <volwahsenl@gmail.com>
2022-11-12 12:19:15 +0100

    b

--- car-app/src/components/Car.js
+++ car-app/src/components/Car.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactSpeedometer from "react-d3-speedometer";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 export default function Car({ state, dispatch }) {
     return (
         <div className="car">
